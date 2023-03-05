7fca764316eaf6d1c548cd26bd7bc10a045a0021
pabloh12345 <108029256+pabloh12345@users.noreply.github.com>
2023-03-04 23:35:27 -0500

    Add files via upload

---
+++ assets/js/weather.js
@@ -0,0 +1,15 @@
+
+import { weather_data } from './data.js';
+
+let loadDayForecastData = () => {
+	
+}
+
+let loadWeekForecastData = () => {
+	
+	
+}
+
+
+loadDayForecastData();
+loadWeekForecastData();
